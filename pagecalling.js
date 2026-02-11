50349fa058080a189d94513f7eecb60d00d93850
Sameer Kamal <sam388030@gmail.com>
2026-02-12 00:00:03 +0530

    added index.html

---
+++ pagecalling.js
@@ -0,0 +1,35 @@
+function fun1(){
+    window.location.href="index1.html";
+}
+
+function fun2(){
+    window.location.href="index2.html";
+}
+function btech(){
+    window.location.href="Btech.html";
+}
+function pharmacy(){
+    window.location.href="pharmacy.html";
+}
+function nursing(){
+    window.location.href="nursing.html";
+}
+function paramedical(){
+    window.location.href="paramedical.html";
+}
+function bca(){
+    window.location.href="bca.html";
+}
+function mca(){
+    window.location.href="mca.html";
+}
+function mba(){
+    window.location.href="mba.html";
+}
+function bba(){
+    window.location.href="bba.html";
+}
+
+function bphorm(){
+    window.location.href="bphorm.html";
+}
